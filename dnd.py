2cefff8633ac253689bc21fa2371ac80a604d114
resident72 <165475039+resident72@users.noreply.github.com>
2025-05-08 06:34:53 +0200

    dnd.py

--- dnd.py
+++ dnd.py
@@ -1,11 +1,11 @@
 import os, time, random
 from colorama import Fore, Style, init
-
 init()
 
 def clear(timer=0):
     time.sleep(timer)
     os.system("cls")
+
 
 class Player:
     def __init__(self, name, health, strength, defense, dexterity):
@@ -17,46 +17,47 @@ class Player:
 
     def __str__(self):
         return f"{self.name}, {self.strength}, {self.health}, {self.defense}, {self.dexterity}"
-
-
-
-def choices(choice_list):
-    for element in choice_list: print(element)
-    while True:
-        try:
-            choice = int(input("What you wanna do? ")) - 1
-            choice_list[choice]
-        except IndexError:
-            continue
-        return choice
-     
-choice = choices([f"[1] {Fore.WHITE}Vanguard: Health: {Fore.GREEN}25{Style.RESET_ALL}  Strength: {Fore.RED}15{Style.RESET_ALL}  Defense: {Fore.BLUE}25{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}10{Style.RESET_ALL}",
-                  f"[2] {Fore.WHITE}Warrior: Health: {Fore.GREEN}20{Style.RESET_ALL}  Strength: {Fore.RED}20{Style.RESET_ALL}  Defense: {Fore.BLUE}15{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}15{Style.RESET_ALL}",
-                  f"[3] {Fore.WHITE}Thief: Health: {Fore.GREEN}15{Style.RESET_ALL}  Strength: {Fore.RED}10{Style.RESET_ALL}  Defense: {Fore.BLUE}10{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}30{Style.RESET_ALL}",
-                  f"[4] {Fore.WHITE}Slave: Health: {Fore.GREEN}10{Style.RESET_ALL}  Strength: {Fore.RED}5{Style.RESET_ALL}  Defense: {Fore.BLUE}5{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}5{Style.RESET_ALL}"])
-
-print("Ich scheiss 🤢 auf FSST, ich  ❤❤❤❤ DIC")
-
-print(choice)
-
-if choice == 0:
-    player = Player("Vanguard", 25, 15, 25, 10)
-elif choice == 1:
-    player = Player("Warrior", 20, 20, 15, 15)
-elif choice == 2:
-    player = Player("Thief", 15, 10, 10, 30)
-elif choice == 3:
-    player = Player("Slave", 10, 5, 5, 5)
-else:
-    print("[!]DUMBASS YOU GOT TO SAY 1 OR 2, HOW CLUELESS CAN YOU GET?[!]")
-
-print(f"You chose the {player.name}!\n")
-clear(2)
-
+    
+    
 def print_stats():
     print(f"Health: {player.health}\nStrength: {player.strength}\nDefense: {player.defense}\nDexterity: {player.dexterity}")
 
 
+def choices(choice_list):                                                   #temporary variable in the function (parameter)
+    for parameter in choice_list: print(parameter)
+    choice = -1
+    while choice not in range(1, len(choice_list) + 1):
+        choice = input("What you wanna do? ")
+        if choice.isdigit():
+            choice = int(choice)
+            print(f"You chose: [{choice}]")   
+            if choice not in range(1, len(choice_list) + 1):
+                print("!Invalid choice, please use the numbers provided below!")   
+                choice = -1                     
+        else:   
+            choice = -1
+            print("!Invalid choice, please use the numbers provided below!")
+    return choice - 1
+
+characters = [
+    Player("Vanguard", 25, 15, 25, 10),
+    Player("Warrior", 20, 20, 15, 15),
+    Player("Thief", 15, 10, 10, 30),
+    Player("Slave", 10, 5, 5, 5)
+]
+
+character_choices= [
+f"[1] {Fore.WHITE}Vanguard: Health: {Fore.GREEN}25{Style.RESET_ALL}  Strength: {Fore.RED}15{Style.RESET_ALL}  Defense: {Fore.BLUE}25{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}10{Style.RESET_ALL}",
+f"[2] {Fore.WHITE}Warrior: Health: {Fore.GREEN}20{Style.RESET_ALL}  Strength: {Fore.RED}20{Style.RESET_ALL}  Defense: {Fore.BLUE}15{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}15{Style.RESET_ALL}",
+f"[3] {Fore.WHITE}Thief: Health: {Fore.GREEN}15{Style.RESET_ALL}  Strength: {Fore.RED}10{Style.RESET_ALL}  Defense: {Fore.BLUE}10{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}30{Style.RESET_ALL}",
+f"[4] {Fore.WHITE}Slave: Health: {Fore.GREEN}10{Style.RESET_ALL}  Strength: {Fore.RED}5{Style.RESET_ALL}  Defense: {Fore.BLUE}5{Style.RESET_ALL}  Dexterity: {Fore.YELLOW}5{Style.RESET_ALL}"
+]
+
+choice = choices(character_choices)
+
+player = characters[choice]
+
+print(f"You chose the {player.name}!")
 
 
 
@@ -79,7 +80,15 @@ while True:
 
 
 
-
+    '''
+    while True:       
+        try:
+            choice = int(input("What you wanna do? ")) - 1
+            choice_list[choice]
+        except IndexError:
+            continue
+        return choice
+'''
 
 '''
 classes = {
